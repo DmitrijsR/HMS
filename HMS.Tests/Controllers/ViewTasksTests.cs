c610b4e218108f94648da50c308234a7c5572ecd
DmitrijsR <dan-5@inbox.lv>
2014-01-16 15:08:16 +0100

    ToDoList

---
+++ HMS.Tests/Controllers/ViewTasksTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using HMS;
+using HMS.Controllers;
+
+namespace HMS.Tests.Controllers
+{
+    [TestClass]
+    public class ViewTasksTests
+    {
+        [TestMethod]
+        public void ViewTasks_Index_is_not_Null()
+        {
+            // Arrange
+            ViewTasksController controller = new ViewTasksController();
+ 
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ HMS/Controllers/CreateToDoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HMS.Models;
+using System.Data.Entity.Core.Objects;
+
+namespace HMS
+{
+    public class CreateToDoController : Controller
+    {
+        //
+        // GET: /CreateToDo/
+        public ActionResult Index()
+        {
+            var model = new ToDoList();
+
+            using (var DB = new DBDataContext())
+            {
+                model.TaskTypes = DB.F_TaskTypes("En").ToList().Select(x => new ToDoList.TaskType
+                {
+                    id = x.ID,
+                    Text = x.Text,
+                    HasInstr = x.HasInstr
+                });
+                
+                model.Patients = DB.F_Patients().ToList().Select(x => new SelectListItem
+                {
+                    Value = x.ID.ToString(),
+                    Text = x.Surname + " " + x.Name + " (" + x.Department + ")"
+                });
+
+                model.PriorityTypes = DB.F_PriorityTypes("En").ToList().Select(x => new SelectListItem
+                {
+                    Value = x.ID.ToString(),
+                    Text = x.Text
+                });
+
+                model.NotificationTypes = DB.F_NotificationTypes("En").ToList().Select(x => new SelectListItem
+                {
+                    Value = x.ID.ToString(),
+                    Text = x.Text
+                });
+
+                model.Dictionary = DB.F_Dictionary("En").ToDictionary(k => k.Tag, v => v.Text);
+            }
+
+            return View("Index", model);
+        }
+
+        public ActionResult Create(ToDoList model)
+        {
+            if (ModelState.IsValid)
+            {
+                //The model is OK. We can do whatever we want to do with the model
+                using (var DB = new DBDataContext())
+                {
+                    foreach (ToDoList.TaskItem ti in model.Tasks)
+                    {
+                        var output = new ObjectParameter("NewTaskID", typeof(int));
+                        //change 1 to insert current user id
+                        DB.SP_Tasks_Add(ti.TaskID, model.PatientID, ti.Importance, ti.Value, 1, output);
+
+                        // Add notification rules using output.Value
+                    }
+                }
+
+                return RedirectToAction("Index", "ViewTasks");
+            }
+            
+            return View("Create");
+        }
+	}
+}

f781e893f3bc25869c015ef85df955a7515f1514
Mbalins <matiss.balins@gmail.com>
2014-02-12 12:38:30 +0200

    Task Assignment now is available from Task Viewer (only for Headnurses); Task Details changes have beem rollbacked

---
+++ HMS/Models/NurseAssign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Models
+{
+    public class NurseAssign
+    {
+        public Int32? TaskID { get; set; }
+        public Int32? NurseID { get; set; }
+        /*
+        public NurseAssign()
+        {
+            this.TaskID = null;
+            this.NurseID = null;
+        }
+        public NurseAssign(Int32? task, Int32? nurse)
+        {
+            this.TaskID = task;
+            this.NurseID = nurse;
+        }
+         * */
+    }
+}
